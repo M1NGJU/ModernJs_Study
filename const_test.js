43d68a20544e60395bef8c131bbbeb4526958b46
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-21 15:15:58 +0900

    const 재선언 불가능

--- const_test.js
+++ const_test.js
@@ -3,5 +3,9 @@ const val3 = "const 변수";
 console.log(val3);
 
 // const 변수는 덮어 쓰기 불가능
-val3 = "const 변수 덮어 쓰기";
+// val3 = "const 변수 덮어 쓰기";
+// console.log(val3);
+
+// const에서의 재선언
+const val3 = "const 변수 재선언"
 console.log(val3);
