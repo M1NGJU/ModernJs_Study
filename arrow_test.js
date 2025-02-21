5490236cf7d2be9f3be05d5d2716737301bf5b5d
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-21 16:01:13 +0900

    기존 함수 사용 예2

--- arrow_test.js
+++ arrow_test.js
@@ -1,9 +1,17 @@
 // 먼저 기존 함수에 대해 알아보자
 
 // 기존 함수 정의
-function fun1(value){
-    return value;
-}
+// function fun1(value){
+//     return value;
+// }
 
-// 실행한 결과 출력
+// // 실행한 결과 출력
+// console.log(fun1("fun1입니다"));
+
+// 기존 함수2
+const fun1 = function(value){
+    return value;
+};
+
+// 실행한 결과를 출력
 console.log(fun1("fun1입니다"));
