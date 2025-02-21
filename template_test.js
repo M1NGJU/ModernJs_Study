2e893f581c06052490b127966a7d3d06337d9823
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-21 15:41:38 +0900

    기존 문자열과 변수 결합 방법

---
+++ template_test.js
@@ -0,0 +1,11 @@
+// 기존 문자열과 변수 결합 방법을 알아보자
+
+// 이름을 저장한 변수
+const name = "김민주";
+
+// 나이를 저장한 변수
+const age = 19;
+
+// 문자열과 변수 결합할 때
+const message = "내 이름은 " + name + "입니다. 나이는 " + age + "세입니다.";
+console.log(message)
