ba0b9df43a81cb7a7fca4dc00aa4c9cfac138404
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-21 14:55:06 +0900

    var 변수 선언

---
+++ var_test.js
@@ -0,0 +1,13 @@
+// var를 이용하여 변수 선언의 문제점을 알아보자
+
+// 변수 덮어 쓰기 및 재선언
+var val1 = "var 변수";
+console.log(val1);
+
+// var 변수는 덮어 쓰기 가능
+var val1 = "var 변수 덮어 쓰기";
+console.log(val1);
+
+// var 변수는 재선언 가능
+var val1 = "var 변수 재선언"
+console.log(val1);
