0af86a71418eada0b12a179b2a8bc11ff91ff5cf
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-24 13:41:20 +0900

    스프레드 구문을 이용해 2개의 배열을 결합

--- spread_test3.js
+++ spread_test3.js
@@ -1,11 +1,20 @@
 // 스프레드 요소 복사와 결합을 알아보자!!
 
 // 2개의 배열 준비
+// const arr4 = [10,20];
+// const arr5 = [30,40];
+
+// arr4의 배열을 복사해 새로운 배열을 생성하자
+// const arr6 = [...arr4];
+
+// console.log(arr4);
+// console.log(arr6);
+
+// 두 개의 배열 결합
 const arr4 = [10,20];
 const arr5 = [30,40];
 
-// arr4의 배열을 복사해 새로운 배열을 생성하자
-const arr6 = [...arr4];
+// 스프레드 구문을 이용해 결합
+const arr7 = [...arr4,...arr5];
 
-console.log(arr4);
-console.log(arr6);
+console.log(arr7);
