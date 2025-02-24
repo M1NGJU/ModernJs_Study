d042e1071d94cf7ae0ea5ac58552e138bda0ce6d
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-24 13:14:18 +0900

    일반적으로 배열값을 전달하는 경우

--- spread_test.js
+++ spread_test.js
@@ -5,6 +5,13 @@
 // console.log(arr1);
 
 // 간단한 스프레드 구문
+// const arr1 = [1,2];
+// console.log(arr1);  // 배열 전개되어 결과 출력
+// console.log(...arr1);   //배열 안의 값이 결과로 출력
+
+// 일반적인 함수와 스프레드 구문 비교
 const arr1 = [1,2];
-console.log(arr1);  // 배열 전개되어 결과 출력
-console.log(...arr1);   //배열 안의 값이 결과로 출력
+const summaryFunc = (num1, num2) => console.log(num1+num2);
+
+// 일반적으로 배열값을 전달하는 경우
+summaryFunc(arr1[0],arr1[1]);
