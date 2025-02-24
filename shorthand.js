538f380c60fa76ef908682fa3a2efc9370979476
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-24 14:05:04 +0900

    속성명과 변수명이 같은 경우 생략 가능 예제

--- shorthand.js
+++ shorthand.js
@@ -6,8 +6,15 @@ const name = "김민주";
 const age = "19";
 
 // user 객체 정의(속성은 name과 age)
+// const user = {
+//     name: name,
+//     age: age,
+// };
+// console.log(user);
+
+// 속성명과 변수명이 같은 경우 예제
 const user = {
-    name: name,
-    age: age,
+    name,
+    age,
 };
 console.log(user);
