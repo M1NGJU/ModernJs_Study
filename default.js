9a9d8a04ffb91316348411f61ed475267e37a400
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-24 12:59:02 +0900

    디폴트 값 설정

--- default.js
+++ default.js
@@ -5,5 +5,11 @@
 // sayHello("김민주");
 
 // 실행 시 인수가 전달되지 않은 경우
-const sayHello = (name) => console.log(`${name}님, 안녕하세요!`);
+// const sayHello = (name) => console.log(`${name}님, 안녕하세요!`);
+// sayHello();
+
+// 디폴트 값 설정
+const sayHello = (name = "게스트") => console.log(`${name}님, 안녕하세요!`);
 sayHello();
+
+// 디폴트 값을 설정하면 인수가 전달되지 않았을 때 디폴트 값을 불러와 보여줄 수 있다.
