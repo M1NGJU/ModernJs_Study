3d09e5a50bc8b2b6c74837792ec15935d773c113
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-24 14:09:28 +0900

    기존 for문 예제

---
+++ map_test.js
@@ -0,0 +1,10 @@
+// map에 대해 알아보자~
+
+// 기존 for문
+// 배열 정의
+const nameArr = ["김민주", "수민", "우영"];
+
+// for문을 이용한 배열 처리
+for(let index = 0; index<nameArr.length; index++){
+    console.log(nameArr[index]);
+}
