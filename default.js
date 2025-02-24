e9dfc2fee8de844457e6d0efc54eece92c7b3da8
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-24 12:57:25 +0900

    실행 시 인수가 전달되지 않은 경우

--- default.js
+++ default.js
@@ -1,5 +1,9 @@
 // 인수의 디폴트 값을 알아보자!
 
 // 메시지를 출력하는 함수
-const sayHello = (name) => console.log(`${name}님, 안녕하세요`);
-sayHello("김민주");
+// const sayHello = (name) => console.log(`${name}님, 안녕하세요`);
+// sayHello("김민주");
+
+// 실행 시 인수가 전달되지 않은 경우
+const sayHello = (name) => console.log(`${name}님, 안녕하세요!`);
+sayHello();
