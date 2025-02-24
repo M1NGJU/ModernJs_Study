e43592b1b09084c37943c0b47b225939fc5b35f3
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-24 15:06:47 +0900

    입력값에 대한 메시지 출력 예제

--- TernaryOperator.js
+++ TernaryOperator.js
@@ -3,5 +3,14 @@
 
 // ?와 :을 이용한 처리 예
 // 1은 0보다 크므로 true, 따라서 :의 왼쪽이 설정
-const val1 = 1 >0 ? "true입니다." : "false입니다.";
-console.log(val1);
+// const val1 = 1 >0 ? "true입니다." : "false입니다.";
+// console.log(val1);
+
+// 입력값에 대한 메시지 출력
+// 수치를 변환해 출력하는 함수
+const printFormattedNum = (num) => {
+    const formattedNum = typeof num === "number"?num.toLocaleString():"숫자를 입력하시오";
+    console.log(formattedNum);
+};
+printFormattedNum(1300);
+printFormattedNum("1300");
