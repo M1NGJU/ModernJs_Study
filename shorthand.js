6c3c4044b55a7b05d7366cd3c6f51f46475ca22b
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-24 14:03:44 +0900

    초기 설정

---
+++ shorthand.js
@@ -0,0 +1,13 @@
+// 쇼트핸드에 대해 알아보자!!
+// -> 객체의 속성명과 설정할 변수명이 같으면 생략 가능!
+
+// 속성명과 변수명이 같은 경우
+const name = "김민주";
+const age = "19";
+
+// user 객체 정의(속성은 name과 age)
+const user = {
+    name: name,
+    age: age,
+};
+console.log(user);
