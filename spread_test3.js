ba559333a5593989c3df6b600245eb7a8999e5ca
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-24 13:39:18 +0900

    스프레드 구문을 이용해 배열을 복사

---
+++ spread_test3.js
@@ -0,0 +1,11 @@
+// 스프레드 요소 복사와 결합을 알아보자!!
+
+// 2개의 배열 준비
+const arr4 = [10,20];
+const arr5 = [30,40];
+
+// arr4의 배열을 복사해 새로운 배열을 생성하자
+const arr6 = [...arr4];
+
+console.log(arr4);
+console.log(arr6);
