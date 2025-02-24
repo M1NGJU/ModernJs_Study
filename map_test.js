51c3634a84c41f454f4e22a8a5ff315e5f15266e
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-24 14:12:47 +0900

    map이용 방법1

--- map_test.js
+++ map_test.js
@@ -2,9 +2,14 @@
 
 // 기존 for문
 // 배열 정의
-const nameArr = ["김민주", "수민", "우영"];
+// const nameArr = ["김민주", "수민", "우영"];
 
-// for문을 이용한 배열 처리
-for(let index = 0; index<nameArr.length; index++){
-    console.log(nameArr[index]);
-}
+// // for문을 이용한 배열 처리
+// for(let index = 0; index<nameArr.length; index++){
+//     console.log(nameArr[index]);
+// }
+
+// map함수 이용 방법
+const nameArr= ["김민주", "수민", "우영"];
+// 배열.map()으로 이용
+const nameArr2 = nameArr.map();
