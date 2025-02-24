cd44a75fa4b70537ffebb428d180c45ae3b2f677
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-24 13:12:00 +0900

    간단한 스프레드 구문

--- spread_test.js
+++ spread_test.js
@@ -1,5 +1,10 @@
 // 스프레드 구문에 대하여 알아보자!
 
 // 배열 준비
+// const arr1 = [1,2];
+// console.log(arr1);
+
+// 간단한 스프레드 구문
 const arr1 = [1,2];
-console.log(arr1);
+console.log(arr1);  // 배열 전개되어 결과 출력
+console.log(...arr1);   //배열 안의 값이 결과로 출력
