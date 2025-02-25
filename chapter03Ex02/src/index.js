673656b288cda81a921ae22c023dea020c078995
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-25 14:59:16 +0900

    div 태그 아래에 button태그를 추가

--- chapter03Ex02/src/index.js
+++ chapter03Ex02/src/index.js
@@ -1,8 +1,12 @@
 // 버튼 설정
 // 버튼 태그 생성
 const buttonEl = document.createElement("button");
+
 // 버튼 라벨 설정
 buttonEl.textContent = "버튼";
 
 // 영역 1의 div 태그 얻기
 const div1El = document.querySelector(".container");
+
+// div 태그 아래에 button 태그를 추가
+div1El.appendChild(buttonEl);
