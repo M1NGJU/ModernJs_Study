7208c2db0931d0921ac0c4aa34de0dec713bae39
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-25 14:30:13 +0900

    querySelector 사용하기

--- chapter03/src/index.js
+++ chapter03/src/index.js
@@ -3,3 +3,7 @@ console.log("test");
 // getElementById 사용
 const title1 = document.getElementById("title");
 console.log(title1);
+
+// querySelector 사용
+const title2 = document.querySelector("#title");
+console.log(title2);
