a94e355069f6ca4c942c416d04271a1073689d8b
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-25 15:04:49 +0900

    body아래 모두 삭제하기

--- chapter03Ex02/src/index.js
+++ chapter03Ex02/src/index.js
@@ -13,10 +13,18 @@ div1El.appendChild(buttonEl);
 
 // DOM 삭제 예제
 // h1 태그 삭제
-const h1El = document.getElementById("title");
+// const h1El = document.getElementById("title");
 
+// body 태그 얻기
+// const bodyEl = document.querySelector("body");
+
+// body 태그 아래부터 삭제
+// bodyEl.removeChild(h1El);
+
+
+// body 아래 모두 삭제하기
 // body 태그 얻기
 const bodyEl = document.querySelector("body");
 
-// body 태그 아래부터 삭제
-bodyEl.removeChild(h1El);
+// body태그 아래부터 삭제
+bodyEl.textContent = null;
