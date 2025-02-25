1d591e6873cc1c1f9d113360521d382e88bcb74e
M1NGJU <s2303@e-mirim.hs.kr>
2025-02-25 14:35:42 +0900

    클래스명으로 엘리먼트 얻기

--- chapter03/src/index.js
+++ chapter03/src/index.js
@@ -7,3 +7,8 @@ console.log(title1);
 // querySelector 사용
 const title2 = document.querySelector("#title");
 console.log(title2);
+
+//클래스명으로 엘리먼트 얻기
+// getElementsByClassName 사용
+const containers = document.getElementsByClassName("container");
+console.log(containers);
